012cd3f225f5af0355a54bf28bf26471de5e3a99
brlvldvlsmrtnz@gmail.com <brlvldvlsmrtnz@gmail.com>
2017-12-29 19:07:27 +0100

    Algunos cambios de estilo y separa los tests

--- src/cylindricalwarp.py
+++ src/cylindricalwarp.py
@@ -100,16 +100,25 @@ def mosaicoN(lista_imagenes):
         coincidencias.append(matches[i])
 
         # Extraigo los keypoints de la imagen i que están en correspondencia con los keypoints de la imagen i+1
-        keypoints_imagen1 = np.float32([keypoints[i][j.queryIdx] for j in matches])
-        keypoints_imagen2 = np.float32([keypoints[i+1][j.trainIdx] for j in matches])
+        keypoints_imagen1 = np.float32([
+            keypoints[i][j.queryIdx]
+            for j in matches
+        ])
+
+        keypoints_imagen2 = np.float32([
+            keypoints[i+1][j.trainIdx]
+            for j in matches
+        ])
 
         # Calcular la homografía entre los dos conjuntos de puntos
-        h, status = (cv2.findHomography(srcPoints=keypoints_imagen1, dstPoints=keypoints_imagen2, method=cv2.RANSAC, ransacReprojThreshold=1))
-        homografia.append(h)
+        h, status = cv2.findHomography(
+            srcPoints=keypoints_imagen1,
+            dstPoints=keypoints_imagen2,
+            method=cv2.RANSAC,
+            ransacReprojThreshold=1
+        )
 
-        # Borramos el contenido de dichos keypoints para usarlos la siguiente iteracion
-        np.array([row for row in keypoints_imagen1 if len(row)<=3])
-        np.array([row for row in keypoints_imagen2 if len(row)<=3])
+        homografia.append(h)
 
     # Nos creamos un fondo negro, con un tamaño específico (para que quepan las demás fotografías)
     ancho = lista_imagenes[0].shape[0]*4
@@ -119,47 +128,70 @@ def mosaicoN(lista_imagenes):
     centro = floor(len(lista_imagenes) / 2)
 
     # Definimos la traslacion que nos pone la imagen central del mosaico en el centro
-    tras = np.matrix([[1, 0, lista_imagenes[centro].shape[1]], [0, 1, lista_imagenes[centro].shape[0]], [0, 0, 1]], dtype=np.float32)
+    tras = np.matrix([
+        [1, 0, lista_imagenes[centro].shape[1]],
+        [0, 1, lista_imagenes[centro].shape[0]],
+        [0, 0, 1]
+    ], dtype=np.float32)
+
     # Llevamos esa imagen al centro de nuestro mosaico con la homografia
-    mosaico = cv2.warpPerspective(src=lista_imagenes[centro], M=tras, dsize=(ancho, alto), flags= cv2.INTER_LINEAR,
-                                  borderMode=cv2.BORDER_TRANSPARENT)
+    mosaico = cv2.warpPerspective(
+        src=lista_imagenes[centro],
+        M=tras,
+        dsize=(ancho, alto),
+        flags= cv2.INTER_LINEAR,
+        borderMode=cv2.BORDER_TRANSPARENT
+    )
 
     # Calculamos las homografias que se le aplican a las imagenes de la izquierda de la imagen central
     for i in range(0, centro):
 
         # Definimos la traslacion para las imágenes de la izquierda
-        izquierda = np.matrix([[1, 0, 0],[0, 1, 0],[0, 0, 1]], dtype=np.float32)
-        #izquierda = np.eye(3, dtype=np.float32)
+        izquierda = np.matrix(np.eye(3, dtype=np.float32))
 
         for j in range(i, centro): izquierda = homografia[j] * izquierda
 
         # Las llevamos al mosaico
-        cv2.warpPerspective(src=lista_imagenes[i], M=tras*izquierda, dst=mosaico, dsize=(ancho, alto), flags= cv2.INTER_LINEAR,
-                            borderMode=cv2.BORDER_TRANSPARENT)
+        cv2.warpPerspective(
+            src=lista_imagenes[i],
+            M=tras * izquierda,
+            dst=mosaico,
+            dsize=(ancho, alto),
+            flags=cv2.INTER_LINEAR,
+            borderMode=cv2.BORDER_TRANSPARENT
+        )
 
     # Calculamos las homografias que se le aplican a las imagenes de la derecha de la imagen central
     # Ahora debemos usar las inversas de las homografías, ya que las homografias que se usan son de la imagen i a la i-1
     for i in range(centro + 1, len(lista_imagenes)):
 
         # Definimos la traslacion para las imágenes de la derecha
-        derecha = np.matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=np.float32)
-        #derecha = np.eye(3, dtype=np.float32)
+        derecha = np.matrix(np.eye(3, dtype=np.float32))
 
         for j in range(centro, i): derecha = derecha * np.linalg.inv(homografia[j])
 
         # Las llevamos al mosaico
-        cv2.warpPerspective(lista_imagenes[i], M=tras*derecha, dst=mosaico, dsize=(ancho, alto), flags= cv2.INTER_LINEAR,
-                            borderMode=cv2.BORDER_TRANSPARENT)
+        cv2.warpPerspective(
+            lista_imagenes[i],
+            M=tras * derecha,
+            dst=mosaico,
+            dsize=(ancho, alto),
+            flags= cv2.INTER_LINEAR,
+            borderMode=cv2.BORDER_TRANSPARENT
+        )
 
     return mosaico
 
 
 def test_warp():
     """Computes and displays a cylindrical warp over a white image"""
-
     img = np.ones((600, 600))
     mondrian = imread('../images/mondrian.jpg')
 
+    show(cylindrical_warp(img, f=600))
+    show(cylindrical_warp(mondrian, f=400))
+
+def test_guernica():
     guernicas = [
         imread('../images/guernica{}.jpg'.format(str(i)))
         for i in range(1, 7)
@@ -170,7 +202,13 @@ def test_warp():
         for guernica in guernicas
     ]
 
+    plot_images([
+        Image(img=w_guernica, title="guernica")
+        for w_guernica in warped_guernicas
+    ])
+
     mosaico = mosaicoN(warped_guernicas)
+
     show(mosaico)
 
 
