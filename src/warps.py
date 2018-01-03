2c3c4f27130b0d73177c8ffac1496c9eb09e9c93
Gecofer <gema@MacBook-Pro-de-Gema.local>
2018-01-03 20:45:54 +0100

    Código comentado

--- src/warps.py
+++ src/warps.py
@@ -1,85 +1,94 @@
-import cv2
 import numpy as np
 
 from math import floor, sqrt
 
+
 def cylindrical_warp(img, f=20):
     """Performs a cylindrical warp on a given image"""
 
-    # la implementacion viene de
-    # https://stackoverflow.com/questions/12017790/warp-image-to-appear-in-cylindrical-projection
-
+    # obtener las coordenadas de la proyección cilíndrica
+    # libro Szeliski: formula 9.13 y 9.14
+    # r=f gives less distortion
     def convert_coordinates(new_point, new_shape, f, r):
 
-        # r=f gives less distortion
+        # x' = r * arctang((x - x_centro) / f) + x_centro
+        # y' = r * ((y - y_centro) / ((x - x_centro)**2 + f**2)) + y_centro
 
         y, x = (
             new_point[0] - new_shape[0]/2,
             new_point[1] - new_shape[1]/2
         )
 
-        new_y = f * ( y / sqrt(x**2 + f**2))
-        new_x = f * np.arctan(x / f)
+        new_y = r * (y / sqrt(x**2 + f**2))
+        new_x = r * np.arctan(x / f)
 
         return (
             floor(new_y) + new_shape[0]//2,
             floor(new_x) + new_shape[1]//2
         )
 
-    # Obtenemos el ancho y el alto de la imagen
-    height, width = img.shape[:2]
-
+    # nos creamos un lienzo para pegar la
+    # proyección cilindrica de la imagen
     new_img = np.zeros(img.shape, dtype=np.uint8)
 
-    for i in range(len(img)):
-        for j in range(len(img[0])):
+    # recorremos la imagen de entrada (matriz)
+    for row_index in range(len(img)):
+        for col_index in range(len(img[0])):
 
+            # convertimos las coordenadas de la
+            # imagen
             y, x = convert_coordinates(
-                (i, j),
+                (row_index, col_index),
                 img.shape[:2],
                 f,
                 f
             )
 
-            new_img[y, x] = img[i, j]
+            new_img[y, x] = img[row_index, col_index]
 
     return new_img
 
 def spherical_warp(img, f=20):
     """Performs a spherical warp on a given image"""
 
+    # obtener las coordenadas de la proyección esférica
+    # libro Szeliski: formula 9.18 y 9.19
+    # r=f gives less distortion
     def convert_coordinates(new_point, new_shape, f, r):
 
-        # r=f gives less distortion
+        # x' = r * arctang((x - x_centro) / f) + x_centro
+        # y' = r * arctang( ((y - y_centro) / ((x - x_centro)**2 + f**2)) ) + y_centro
 
         y, x = (
             new_point[0] - new_shape[0]/2,
             new_point[1] - new_shape[1]/2
         )
 
-        new_y = f * np.arctan( y / sqrt(x**2 + f**2))
-        new_x = f * np.arctan(x / f)
+        new_y = r * np.arctan( y / sqrt(x**2 + f**2))
+        new_x = r * np.arctan(x / f)
 
         return (
             floor(new_y) + new_shape[0]//2,
             floor(new_x) + new_shape[1]//2
         )
 
-    # Obtenemos el ancho y el alto de la imagen
-    height, width = img.shape[:2]
-
+    # nos creamos un lienzo para pegar la
+    # proyección esférica de la imagen
     new_img = np.zeros(img.shape, dtype=np.uint8)
 
-    for i in range(len(img)):
-        for j in range(len(img[0])):
+    # recorremos la imagen de entrada (matriz)
+    for row_index in range(len(img)):
+        for col_index in range(len(img[0])):
 
+            # convertimos las coordenadas de la
+            # imagen
             y, x = convert_coordinates(
-                (i, j),
+                (row_index, col_index),
                 img.shape[:2],
                 f,
                 f
             )
 
-            new_img[y, x] = img[i, j]
+            new_img[y, x] = img[row_index, col_index]
 
     return new_img
