dcaa55dc183cf8b8ebebcd5efa881fbc2a575ad0
Gecofer <gema@MacBook-Pro-de-Gema.local>
2018-01-08 18:31:53 +0100

    Burt-Adelson no funciona

--- src/burt_adelson.py
+++ src/burt_adelson.py
@@ -3,91 +3,103 @@ import numpy as np
 
 from math import floor, sqrt
 from util import show
+from matplotlib.pyplot import imread
+
 
 # Implementación: https://github.com/stheakanath/multiresolutionblend/blob/master/main.py
-
-def burt_adelson(imgA, imgB, mask):
-    """Algoritmo de Burt-Adelson"""
-
-    gaussian_mask = compute_gaussian_pyramid(mask, levels=4)
-
-    # calculamos la piramide laplaciana de las imagenes
-    # A y B
-    lAs = compute_laplacian_pyramid(imgA.astype(np.float64)/255)
-    lBs = compute_laplacian_pyramid(imgB.astype(np.float64)/255)
-    
-    
-    lSs = []
-    
-    for lA, lB, GR in zip(lAs, lBs, gaussian_mask):
-        r = lA * GR + lB * (1 - GR)
-        show(lA, lB, GR, lA * GR, lB * (1 - GR), r)
-        lSs.append(r)
+# Implementación: https://docs.opencv.org/3.1.0/dc/dff/tutorial_py_pyramids.html
 
 
+def compute_gaussian_pyramid(img, levels=6):
+    downsampled = img.copy()
+    pyramid = [downsampled]
+
+    for i in range(levels):
+        downsampled = cv2.pyrDown(downsampled)
+        pyramid.append(downsampled)
+
+    return pyramid
 
 
-    result = collapse_laplacian_pyramid(lSs)
-    # aqui habria que recomponer las componentes de la laplaciana
-    # lSs en una sola imagen
+def compute_laplacian_pyramid(img, levels=6):
 
-    return result
+    laplacian = compute_gaussian_pyramid(img, levels)
+    pyramid = [laplacian[-1]]
 
-'''
-    # creamos la imagen que contendrá la
-    # combinación de la imagen A y B
-    recomponer_image = np.empty(imgA.shape)
-    
-    # reconstruimos la imagen desde el nivel 
-    # n hasta el nivel 0
-    for lS in lSs:
-        recomponer_image.append(lS + (lS.shape[0], lS.shape[1]))
-        
-    imagen_final = recomponer_image
-	
-	# normalizamos la imagen
-	
-	return imagen_final
-'''
-
-def compute_laplacian_pyramid(img, levels=4):
-    g_pyramid = compute_gaussian_pyramid(img, levels)
-    pyramid = []
-    
-    for imgIzq, imgDer in zip(g_pyramid, g_pyramid[1:]):
-        height, width = imgIzq.shape[:2]
-
-        pyramid.append(
-            np.float64(cv2.addWeighted(
-                np.uint8(imgIzq * 255), 1,
-                np.uint8(cv2.pyrUp(imgDer)[:height, :width] * 255), -1,
-                0
-            ))/255
-        )
-
-    # añadimos el último nivel de la gaussiana
-    pyramid.append(np.float64(g_pyramid[-1]) / 255)
+    for i in range(5,0,-1):
+    #for i in range(len(laplacian)-1)[::-1]:
+        GE = cv2.pyrUp(laplacian[i])
+        L = cv2.subtract(laplacian[i-1],GE)
+        pyramid.append(L)
 
     return pyramid
 
 def collapse_laplacian_pyramid(pyramid):
+
+
     result = np.zeros(pyramid[-1].shape)
 
     for img in reversed(pyramid):
         height, width = img.shape[:2]
 
-        import pdb; pdb.set_trace()
         result = cv2.addWeighted(img, 1, result[:height, :width], 1, 0)
         result = cv2.pyrUp(result)
-    
+
     return result
+    '''
+    lS = pyramid[0]
 
-def compute_gaussian_pyramid(img, levels=4):
-    pyramid = [img]
-    
-    downsampled = img
-    for _ in range(levels):
-        downsampled = cv2.pyrDown(downsampled)
-        pyramid.append(downsampled)
+    for i in range(1,6):
+        lS = cv2.pyrUp(lS)
+        lS = cv2.add(lS, pyramid[i])
 
-    return pyramid
+    return lS
+    '''
+
+def combine_laplacian_pyramids(laplacianA, laplacianB, gaussianMask):
+    blend = []
+
+    for lS in range(len(laplacianB)):
+        blend.append(gaussianMask[lS]*laplacianA[lS] + (1-gaussianMask[lS])*laplacianB[lS])
+
+    return blend
+
+
+def burt_adelson(imgA, imgB, mask):
+
+    # Step la. Build Laplacian pyramids LA and LB
+    # for images A and B respectively.
+    lAs = compute_laplacian_pyramid(imgA)
+    lBs = compute_laplacian_pyramid(imgB)
+
+    # Step lb. Build a Gaussian pyramid GR for the
+    # region image R.
+    gaussian_mask = compute_gaussian_pyramid(mask,
+                                             levels=4)
+
+    # Step 2. Form a combined pyramid LS from LA and LB
+    # using nodes of GR as weights. That is, for each l, i and j:
+    pyramid = combine_laplacian_pyramids(lAs, lBs, gaussian_mask)
+    '''
+    lSs = []
+    for lA, lB, GR in zip(lAs, lBs, gaussian_mask):
+        r = lA * GR + lB * (1 - GR)
+        show(lA, lB, GR, lA * GR, lB * (1 - GR), r)
+        lSs.append(r)
+    '''
+    # aqui habria que recomponer las componentes de la laplaciana
+    img_blend = collapse_laplacian_pyramid(pyramid)
+
+    return img_blend
+
+
+def test_laplacian_pyramid():
+    """Computes and displays a laplacian pyramid"""
+    guernica = imread('../images/guernica3.jpg')
+    pyr = compute_laplacian_pyramid(guernica)
+    reconstructed = collapse_laplacian_pyramid(pyr)
+    show(*pyr, reconstructed)
+
+if __name__ == "__main__":
+    test_laplacian_pyramid()
+
